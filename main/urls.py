89ef11fea7821330438211b523843a9faf2af7a0
Rayyan <rayyanagumilang@gmail.com>
2025-09-09 21:54:28 +0700

    testing

---
+++ main/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from main.views import show_main
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('main.urls')),
+]
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', show_main, name='show_main'),
+]
