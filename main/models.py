9914ff461cae59827f42e03ae3445cd5bb1b7747
Rayyan <rayyanagumilang@gmail.com>
2025-09-09 22:45:10 +0700

    test

--- main/models.py
+++ main/models.py
@@ -11,11 +11,3 @@ class News(models.Model):
     
     def __str__(self):
         return self.title
-    
-    @property
-    def is_news_hot(self):
-        return self.news_views > 20
-        
-    def increment_views(self):
-        self.news_views += 1
-        self.save()
