9be81a091827f1197faa9d6c048fadceb89f6d1a
Rayyan <rayyanagumilang@gmail.com>
2025-09-09 22:46:26 +0700

    test

--- main/models.py
+++ main/models.py
@@ -1,13 +1,10 @@
 import uuid
 from django.db import models
 
-class News(models.Model):
+class Shop(models.Model):
     
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.URLField(blank=True, null=True)
     is_featured = models.BooleanField(default=False)
-    
-    def __str__(self):
-        return self.title
